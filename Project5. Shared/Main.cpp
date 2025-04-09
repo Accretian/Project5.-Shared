1c90412eef7ff3084fc7cee2d141facd591bd5de
Accretian <kvik.ocean@gmail.com>
2025-04-09 14:32:08 +0300

    1

--- Project5. Shared/Main.cpp
+++ Project5. Shared/Main.cpp
@@ -1,17 +1,19 @@
-#include <cstdlib>
+#include <cmath>
 #include <iostream>
 
 int main() {
-	int a1, b1, a2, b2;
-	std::cin >> a1 >> b1 >> a2 >> b2;
+    int a1, b1, a2, b2;
+    std::cin >> a1 >> a2 >> b1 >> b2;
+
+    std::string out = (abs(a1 - a2) == abs(b1 - b2) || )
 
 
 
 
-	 
-	 
 
-	return 0;
+
+
+
 
 
 }
