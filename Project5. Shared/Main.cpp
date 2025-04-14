5b943632a9f9a92002634d9ff2950f768106241e
Accretian <kvik.ocean@gmail.com>
2025-04-14 14:54:10 +0300

    1

--- Project5. Shared/Main.cpp
+++ Project5. Shared/Main.cpp
@@ -1,11 +1,44 @@
-#include <cmath>
 #include <iostream>
 
 int main() {
-    int a1, b1, a2, b2;
-    std::cin >> a1 >> a2 >> b1 >> b2;
 
-    std::string out = (abs(a1 - a2) == abs(b1 - b2) || )
+    int month = 0;
+    int year = 0;
+
+    std::cin >> month >> year;
+
+    std::string leap = "undefined";
+    int days = 0;
+
+    if ((year % 400) == 0) {
+        leap = "YES";
+    }
+    else if ((year % 100) == 0) {
+        leap = "NO";
+    }
+    else if ((year % 4) == 0) {
+        leap = "YES";
+    }
+    else leap = "NO";
+
+    if (month == 2) {
+        if (leap == "YES") {
+            days = 29;
+        }
+        else days = 28;
+    }
+    else if ((month % 2) == 0) {
+        days = 30;
+    }
+    else if ((month % 2) == 1) {
+        days = 31;
+    }
+
+
+
+
+    std::cout << days;
+
 
 
 
