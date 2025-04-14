ba71b5e1be74752708156f1ce5f51166a9406be8
Accretian <kvik.ocean@gmail.com>
2025-04-14 23:45:51 +0300

    1

--- Project5. Shared/Main.cpp
+++ Project5. Shared/Main.cpp
@@ -7,19 +7,22 @@ int main() {
 
 	std::cin >> weekDay >> daysCount;
 
-
-
 	for (int i = 1; i <= daysCount;) {
 		for (int j = 1; j <= 7; j++) {
 			if (j == weekDay) {
 				if (i <= daysCount) {
-					std::cout << i << '\t';
+					if (i < 10)
+						std::cout << " ";
+					std::cout << i;
+					if (j < 7) {
+						std::cout << " ";
+					}
 					i++;
 					weekDay++;
 				}
 			}
 			else
-				std::cout << '\t';
+				std::cout << "   ";
 		}
 		weekDay = 1;
 		std::cout << '\n';
