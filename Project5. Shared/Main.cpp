5a89be81962360048061e5a46d23481587e09780
Accretian <kvik.ocean@gmail.com>
2025-04-14 16:30:37 +0300

    1

--- Project5. Shared/Main.cpp
+++ Project5. Shared/Main.cpp
@@ -2,46 +2,21 @@
 
 int main() {
 
-    int month = 0;
-    int year = 0;
+	int weekDay = 0;
+	int daysCount = 0;
+	int spaces = 0;
 
-    std::cin >> month >> year;
+	std::cin >> weekDay >> daysCount;
 
-    std::string leap = "undefined";
-    int days = 0;
-
-    if ((year % 400) == 0) {
-        leap = "YES";
-    }
-    else if ((year % 100) == 0) {
-        leap = "NO";
-    }
-    else if ((year % 4) == 0) {
-        leap = "YES";
-    }
-    else leap = "NO";
-
-    if (month == 2) {
-        if (leap == "YES") {
-            days = 29;
-        }
-        else days = 28;
-    }
-    else if (month < 8) {
-        if ((month % 2) == 0) {
-            days = 30;
-        }
-        else days = 31;
-    }
-    else if ((month % 2) == 0) {
-        days = 31;
-    }
-    else days = 30;
+	for (int i = 1; i <= daysCount; i++) {
+		
 
 
 
 
-    std::cout << days;
+
+
+	}
 
 
 
@@ -49,6 +24,11 @@ int main() {
 
 
 
+
+
+	int dontCloseThisProgram;
+
+	std::cin >> dontCloseThisProgram;
 
 
 
